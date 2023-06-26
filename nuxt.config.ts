3621f1f808377333469239b97c2aa796a75be86f
baiyan412048 <39880236+baiyan412048@users.noreply.github.com>
2023-06-26 19:46:18 +0800

    use @nuxtjs/google-fonts modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxt-icon', '@nuxt/image'],
+  modules: ['nuxt-icon', '@nuxt/image', '@nuxtjs/google-fonts'],
   css: ['@/assets/base/_reset.sass'],
   app: {
     head: {
-      link: [
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.googleapis.com'
-        },
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.gstatic.com'
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Concert+One&family=Noto+Sans+JP:wght@500&family=Noto+Serif+JP:wght@900&family=Pirata+One&display=swap'
-        }
-      ],
       meta: [{ name: 'theme-color', content: '#34BD32' }]
+    }
+  },
+  googleFonts: {
+    families: {
+      'Concert+One': true,
+      'Pirata+One': true,
+      'Noto+Sans+JP': [500],
+      'Noto+Serif+JP': [900]
     }
   }
 })
